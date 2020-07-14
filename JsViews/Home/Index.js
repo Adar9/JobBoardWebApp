18cbed3e95e6da49e959e866bcaf931db3c7d67f
Eduardo Cabrera <r.eduardo.cabrera@hotmail.com>
2020-07-14 12:20:20 -0600

    Last update

--- JsViews/Home/Index.js
+++
@@ -1,7 +0,0 @@
-﻿$(document).ready(function () {
-    $(".InfoDelete").click(function (event) {
-        if (!confirm('Do you want to delete the record?')) {
-            event.preventDefault();
-        }
-    })
-})
